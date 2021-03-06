d0c0268e0b230b81959c1adb3af9f59022da467e
Chauhan Shivani <shivanichauhan@DLLA071P4174.qait.com>
2021-03-06 21:55:45 +0530

    My News App Code

---
+++ lib/LoginFormWithValidation.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+import 'HomePage.dart';
+
+class LoginFormValidation extends StatefulWidget {
+  @override
+  _LoginFormValidationState createState() => _LoginFormValidationState();
+}
+
+class _LoginFormValidationState extends State<LoginFormValidation> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
+  String validatePassword(String value) {
+    if (value.isEmpty) {
+      return "* Required";
+    } else if (value.length < 6) {
+      return "Password should be atleast 6 characters";
+    } else if (value.length > 10) {
+      return "Password should not be greater than 10 characters";
+    } else
+      return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          autovalidate: true, //check for validation while typing
+          key: formkey,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0),
+                child: Center(
+                  child: Container(
+                      width: 200,
+                      height: 150,
+                      child: Image.asset('asset/images/flutter-logo.png')),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Enter valid email id as abc@gmail.com'),
+                    validator: MultiValidator([
+                      RequiredValidator(errorText: "* Required"),
+                      EmailValidator(errorText: "Enter valid email id"),
+                    ])),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 15.0, right: 15.0, top: 15, bottom: 0),
+                child: TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter secure password'),
+                    validator: MultiValidator([
+                      RequiredValidator(errorText: "* Required"),
+                      MinLengthValidator(6,
+                          errorText: "Password should be atleast 6 characters"),
+                      MaxLengthValidator(10,
+                          errorText:
+                          "Password should not be greater than 10 characters")
+                    ])
+                  //validatePassword,        //Function to check validation
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  //TODO FORGOT PASSWORD SCREEN GOES HERE
+                },
+                child: Text(
+                  'Forgot Password',
+                  style: TextStyle(color: Colors.blue, fontSize: 15),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20)),
+                child: FlatButton(
+                  onPressed: () {
+                    if (formkey.currentState.validate()) {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => MyTabbedPage()));
+                      print("Validated");
+                    } else {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => AlertDialog(
+                            title: Text("Invalid User"),
+                            content: Text("Please enter valid email and password."),
+                            actions: <Widget>[
+                              FlatButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text("OK"),
+                              ),
+                            ],
+                          ),
+                          ));
+                      print("Not Validated");
+                    }
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Text('New User? Create Account')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'SplashPage.dart';
+
+import 'HomePage.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News Application',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SplashPage(),
+    );
+  }
+}
